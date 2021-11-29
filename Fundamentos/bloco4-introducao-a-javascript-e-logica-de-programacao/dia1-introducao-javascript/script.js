13eb6f1a9a32c708072040df4eaaf5ac33eb0231
Bruno Bastos <brunomourabastos@gmail.com>
2021-11-29 14:52:23 -0300

    Exerici 1, 5 operacoes

---
+++ Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript/script.js
@@ -0,0 +1,13 @@
+const a = 4;
+const b = 5;
+
+soma = a + b;
+console.log("Soma", soma);
+subtracao = a - b;
+console.log("Subtracao", subtracao);
+multiplicacao = a * b;
+console.log("Multiplicacao", multiplicacao);
+divisao = a / b;
+console.log("Divisao", divisao);
+modulo = a % b;
+console.log("Restante da divisao entre a/b", modulo);
