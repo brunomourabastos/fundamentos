48d60fd55df655396737f05862fc1168b387e768
Bruno Bastos <brunomourabastos@gmail.com>
2021-11-29 14:55:15 -0300

    Exercicio 2, numeros maiores

--- Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript/script.js
+++ Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript/script.js
@@ -11,3 +11,12 @@ divisao = a / b;
 console.log("Divisao", divisao);
 modulo = a % b;
 console.log("Restante da divisao entre a/b", modulo);
+
+const num_a = 9;
+const num_b = 8;
+
+if (num_a > num_b) {
+    console.log(num_a, "é o maior número");
+} else {
+    console.log(num_b, "é o maior número.")
+}
