f38d06e6423942ef99175ce02aba2f3487532cc8
Bruno Bastos <brunomourabastos@gmail.com>
2021-11-29 15:01:08 -0300

    Exericio 4 - Positivo e negativo

--- Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript/script.js
+++ Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript/script.js
@@ -21,15 +21,23 @@
 //     console.log(num_b, "é o maior número.")
 // }
 
-const maior_a = 4;
-const maior_b = 2;
-const maior_c = 3;
+// const maior_a = 4;
+// const maior_b = 2;
+// const maior_c = 3;
 
-if (maior_a > maior_b && maior_a > maior_c) {
-    console.log("Maior número é:", maior_a);
-} else if (maior_b > maior_a && maior_b > maior_c) {
-    console.log("Maior número é:", maior_b);
+// if (maior_a > maior_b && maior_a > maior_c) {
+//     console.log("Maior número é:", maior_a);
+// } else if (maior_b > maior_a && maior_b > maior_c) {
+//     console.log("Maior número é:", maior_b);
 
+// } else {
+//     console.log("Maior número é:", maior_c);
+// }
+
+const numeroPositivo = -2;
+
+if (numeroPositivo > 0) {
+    console.log("Positive");
 } else {
-    console.log("Maior número é:", maior_c)
+    console.log("Negative");
 }
