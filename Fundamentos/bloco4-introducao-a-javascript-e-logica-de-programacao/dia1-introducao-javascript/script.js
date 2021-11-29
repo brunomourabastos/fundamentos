fb64376a4997c974a6ab40853a78ee41231a09f6
Bruno Bastos <brunomourabastos@gmail.com>
2021-11-29 14:59:15 -0300

    Exericio 3 - Maior de 3 numeros

--- Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript/script.js
+++ Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript/script.js
@@ -1,22 +1,35 @@
-const a = 4;
-const b = 5;
+// const a = 4;
+// const b = 5;
 
-soma = a + b;
-console.log("Soma", soma);
-subtracao = a - b;
-console.log("Subtracao", subtracao);
-multiplicacao = a * b;
-console.log("Multiplicacao", multiplicacao);
-divisao = a / b;
-console.log("Divisao", divisao);
-modulo = a % b;
-console.log("Restante da divisao entre a/b", modulo);
+// soma = a + b;
+// console.log("Soma", soma);
+// subtracao = a - b;
+// console.log("Subtracao", subtracao);
+// multiplicacao = a * b;
+// console.log("Multiplicacao", multiplicacao);
+// divisao = a / b;
+// console.log("Divisao", divisao);
+// modulo = a % b;
+// console.log("Restante da divisao entre a/b", modulo);
 
-const num_a = 9;
-const num_b = 8;
+// const num_a = 9;
+// const num_b = 8;
 
-if (num_a > num_b) {
-    console.log(num_a, "é o maior número");
+// if (num_a > num_b) {
+//     console.log(num_a, "é o maior número");
+// } else {
+//     console.log(num_b, "é o maior número.")
+// }
+
+const maior_a = 4;
+const maior_b = 2;
+const maior_c = 3;
+
+if (maior_a > maior_b && maior_a > maior_c) {
+    console.log("Maior número é:", maior_a);
+} else if (maior_b > maior_a && maior_b > maior_c) {
+    console.log("Maior número é:", maior_b);
+
 } else {
-    console.log(num_b, "é o maior número.")
+    console.log("Maior número é:", maior_c)
 }
