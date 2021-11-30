30ba498d6ceec65aa4ab826a440712b391d62145
Bruno Bastos <brunomourabastos@gmail.com>
2021-11-30 12:44:41 -0300

    Imprimir mensagem diferente se media for maior que 20 ou menor que 20

--- Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia2-array-e-loop-for/script.js
+++ Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia2-array-e-loop-for/script.js
@@ -1,18 +1,32 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sumNumbers = 0;
 
+//Exericio 1:
 // for (numberIndex = 0; numberIndex < numbers.length; numberIndex += 1) {
 //     console.log(numbers[numberIndex]);
 // }
 
+//Exericio 2:
 // for (number of numbers) {
 //     sumNumbers += number;
 // }
 // console.log(sumNumbers);
 
+//Exercicio 3:
+// for (numbersIndex = 0; numbersIndex < numbers.length; numbersIndex += 1) {
+//     sumNumbers += numbers[numbersIndex];
+// }
+// let mediaNumbers = sumNumbers / numbers.length;
+// console.log("Sua média foi de: ", mediaNumbers);
+
+//Exercicio 4:
 for (numbersIndex = 0; numbersIndex < numbers.length; numbersIndex += 1) {
     sumNumbers += numbers[numbersIndex];
-
 }
 let mediaNumbers = sumNumbers / numbers.length;
 console.log("Sua média foi de: ", mediaNumbers);
+if (mediaNumbers > 20) {
+    console.log("Valor maior que 20.")
+} else {
+    console.log("Valor menor ou igual a 20");
+}
