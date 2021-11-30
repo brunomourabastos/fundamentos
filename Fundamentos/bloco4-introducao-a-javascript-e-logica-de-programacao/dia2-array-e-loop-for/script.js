87ee6f274cf03e8e23355bec702be9795ef66c36
Bruno Bastos <brunomourabastos@gmail.com>
2021-11-30 14:58:16 -0300

    Exercicio 7 - Informar o menor numero da lista

--- Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia2-array-e-loop-for/script.js
+++ Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia2-array-e-loop-for/script.js
@@ -1,10 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sumNumbers = 0;
-let biggestNumber = 0;
+let biggestNumber = numbers[0];
 let oddsNumber = 0;
 
+
 //Exericio 1:
-// for (numberIndex = 0; numberIndex < numbers.length; numberIndex += 1) {
+// for (let numberIndex = 0; numberIndex < numbers.length; numberIndex += 1) {
 //     console.log(numbers[numberIndex]);
 // }
 
@@ -15,14 +16,14 @@ let oddsNumber = 0;
 // console.log(sumNumbers);
 
 //Exercicio 3:
-// for (numbersIndex = 0; numbersIndex < numbers.length; numbersIndex += 1) {
+// for (let numbersIndex = 0; numbersIndex < numbers.length; numbersIndex += 1) {
 //     sumNumbers += numbers[numbersIndex];
 // }
 // let mediaNumbers = sumNumbers / numbers.length;
 // console.log("Sua média foi de: ", mediaNumbers);
 
 //Exercicio 4:
-// for (numbersIndex = 0; numbersIndex < numbers.length; numbersIndex += 1) {
+// for (let numbersIndex = 0; numbersIndex < numbers.length; numbersIndex += 1) {
 //     sumNumbers += numbers[numbersIndex];
 // }
 // let mediaNumbers = sumNumbers / numbers.length;
@@ -34,7 +35,7 @@ let oddsNumber = 0;
 // }
 
 //Exercicio 5:
-// for (numbersIndex = 0; numbersIndex < numbers.length; numbersIndex += 1) {
+// for (let numbersIndex = 0; numbersIndex < numbers.length; numbersIndex += 1) {
 //     if (numbers[numbersIndex] > biggestNumber) {
 //         biggestNumber = numbers[numbersIndex];
 //     }
@@ -42,13 +43,22 @@ let oddsNumber = 0;
 // console.log(biggestNumber);
 
 //Exercicio 6:
-for (numbersIndex = 0; numbersIndex < numbers.length; numbersIndex += 1) {
-    if (numbers[numbersIndex] % 2 == 1) {
-        oddsNumber += 1;
+// for (let numbersIndex = 0; numbersIndex < numbers.length; numbersIndex += 1) {
+//     if (numbers[numbersIndex] % 2 == 1) {
+//         oddsNumber += 1;
+//     }
+// }
+// if (oddsNumber > 0) {
+//     console.log("A quantidade de números ímpares é: ", oddsNumber);
+// } else {
+//     console.log("Nenhum valor ímpar encontrado.");
+// }
+
+//Exercicio 7:
+let lowestNumber = numbers[0];
+for (number of numbers) {
+    if (number < lowestNumber) {
+        lowestNumber = number;
     }
 }
-if (oddsNumber > 0) {
-    console.log("A quantidade de números ímpares é: ", oddsNumber);
-} else {
-    console.log("Nenhum valor ímpar encontrado.");
-}
+console.log(lowestNumber);
