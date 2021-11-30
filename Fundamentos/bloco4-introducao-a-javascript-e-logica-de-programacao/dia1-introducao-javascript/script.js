b3b6028149591ff5d51a9c8cd0acd6dd1aca6af0
Bruno Bastos <brunomourabastos@gmail.com>
2021-11-29 23:11:11 -0300

    Exercicio 6 - Dicionario Xadrez com toLowerCase()

--- Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript/script.js
+++ Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia1-introducao-javascript/script.js
@@ -42,21 +42,67 @@
 //     console.log("Negative");
 // }
 
-const angulo_a = 90;
-const angulo_b = -45;
-const angulo_c = 45;
+// const angulo_a = 90;
+// const angulo_b = -45;
+// const angulo_c = 45;
 
-let soma_angulos = angulo_a + angulo_b + angulo_c;
+// let soma_angulos = angulo_a + angulo_b + angulo_c;
 
-if (soma_angulos === 180) {
-    triangulo = true;
-} else {
-    triangulo = false;
-}
-switch (triangulo) {
-    case true:
-        console.log("Angulos representam um triângulo");
+// if (soma_angulos === 180) {
+//     triangulo = true;
+// } else {
+//     triangulo = false;
+// }
+// switch (triangulo) {
+//     case true:
+//         console.log("Angulos representam um triângulo");
+//         break;
+//     case false:
+//         console.log("Angulos não representam um triângulo");
+// }
+
+// let lista = [1, 2, 3, 4, 5, 6, 7, 8];
+// for (let x in lista) {
+//     console.log(lista[x]);
+// }
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+// console.log(indexOfPortfolio)
+// menu.push('Contato');
+// console.log(menu)
+
+// var numero = 5;
+// for (var contador = 1; contador <= 10; contador++) {
+//     console.log(numero * contador);
+// }
+
+//Programa que informa movimento das peças de xadrez:
+let pecaXadrez = 'cavalo';
+let xadrezPadrao = pecaXadrez.toLowerCase();
+
+switch (xadrezPadrao) {
+    case 'peao':
+        console.log("Anda uma casa e come na diagonal");
         break;
-    case false:
-        console.log("Angulos não representam um triângulo");
+    case 'torre':
+        console.log("Anda somente nas retas");
+        break;
+    case 'cavalo':
+        console.log("Movimentação em L");
+        break;
+    case 'bispo':
+        console.log("Movimentação somente na diagonal");
+        break;
+    case 'rainha':
+        console.log("Movimento livre");
+        break;
+    case 'rei':
+        console.log("Anda somente 1 casa para qualquer direção");
+        break;
+    default:
+        console.log("Peça desconhecida");
 }
