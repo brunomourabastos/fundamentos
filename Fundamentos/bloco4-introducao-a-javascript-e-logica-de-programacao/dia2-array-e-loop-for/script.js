9a3f214ec54d15e9166f4f8f4cc79ed6f5d191cd
Bruno Bastos <brunomourabastos@gmail.com>
2021-11-30 12:35:00 -0300

    Exercicio 2 - Somar todos os valores contidos no array e imprimir resultado

--- Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia2-array-e-loop-for/script.js
+++ Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia2-array-e-loop-for/script.js
@@ -1,11 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sumNumbers = 0;
 
-for (numberIndex = 0; numberIndex < numbers.length; numberIndex += 1) {
-    console.log(numbers[numberIndex]);
-}
-
-
-
-// for (number of numbers) {
-//     console.log(number);
+// for (numberIndex = 0; numberIndex < numbers.length; numberIndex += 1) {
+//     console.log(numbers[numberIndex]);
 // }
+
+for (number of numbers) {
+    sumNumbers += number;
+}
+console.log(sumNumbers);
