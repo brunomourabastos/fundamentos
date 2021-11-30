62cdc39591f8aba8e29e760bc36eed99478edee3
Bruno Bastos <brunomourabastos@gmail.com>
2021-11-30 12:17:27 -0300

    Exercicio 1 - Percorrer imprimindo todos os valores do array. Feito com as 2 formas, simplificada e a amplificada

---
+++ Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia2-array-e-loop-for/script.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (numberIndex = 0; numberIndex < numbers.length; numberIndex += 1) {
+    console.log(numbers[numberIndex]);
+}
+
+
+
+// for (number of numbers) {
+//     console.log(number);
+// }
