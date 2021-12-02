c0c1a877befaf74cbcdce8caaaabe07907d9b182
Bruno Bastos <brunomourabastos@gmail.com>
2021-12-02 00:07:30 -0300

    Exericio 8 - Fazer um console.log com informacoes nome e length de livros favoritos.

--- Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia4-objetos-e-funcoes/script.js
+++ Fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia4-objetos-e-funcoes/script.js
@@ -35,26 +35,44 @@
 // }
 
 //Exercicio 1 - 4.2
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim'
-};
-// console.log("Bom dia ", info.personagem + '!');
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//     recorrente: 'Sim'
+// };
+// // console.log("Bom dia ", info.personagem + '!');
+// // for (key in info) {
+// //     console.log(info[key]);
+// // }
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim'
+// };
 // for (key in info) {
-//     console.log(info[key]);
+//     if (key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim') {
+//         console.log('Ambos recorrentes')
+//     } else {
+//         console.log(info[key] + ' e ' + info2[key])
+//     }
 // }
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim'
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [{
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+    }, ],
 };
-for (key in info) {
-    if (key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim') {
-        console.log('Ambos recorrentes')
-    } else {
-        console.log(info[key] + ' e ' + info2[key])
-    }
-}
+// console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama '" + leitor.livrosFavoritos[0].titulo + "'.");
+leitor.livrosFavoritos.push({
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editor: 'Rocco',
+})
+console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos.");
